1773ba273622dff3d481495c42065c35e50b57c8
praneeth1218 <praneetharceus@gmail.com>
2024-10-30 18:37:30 +0530

    Update users.routes.js

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,13 +1,20 @@
 import express from "express";
-import { login, register, getUserHistory, addToHistory, scheduleMeeting, getScheduledMeetings } from "../controllers/user.controller.js";
+import {
+  login,
+  register,
+  getUserHistory,
+  addToHistory,
+  scheduleMeeting,
+  getScheduledMeetings,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.route("/login").post(login)
-router.route("/register").post(register)
-router.route("/add_to_activity").post(addToHistory)
-router.route("/get_all_activity").get(getUserHistory)
-router.route("/schedule").post(scheduleMeeting)
-router.route("/scheduled-meetings").get(getScheduledMeetings) // New route for fetching scheduled meetings
+router.route("/login").post(login);
+router.route("/register").post(register);
+router.route("/add_to_activity").post(addToHistory);
+router.route("/get_all_activity").get(getUserHistory);
+router.route("/schedule").post(scheduleMeeting);
+router.route("/scheduled-meetings").get(getScheduledMeetings); // New route for fetching scheduled meetings
 
 export default router;
