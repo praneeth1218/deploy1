de235ccdc4674bb8ff5e769b6a15021a759f5426
praneeth1218 <praneetharceus@gmail.com>
2024-10-30 14:26:11 +0530

    Update landing.jsx

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -7,53 +7,47 @@ export default function LandingPage() {
     const router = useNavigate();
 
     return (
-      <div className="landingPageContainer">
-        <nav>
-          <div className="navHeader">
-            <h2>LinkUp</h2>
-          </div>
-          <div className="navlist">
-            <p
-              onClick={() => {
-                router("/aljk23");
-              }}
-            >
-              Join as Guest
-            </p>
-            <p
-              onClick={() => {
-                router("/auth");
-              }}
-            >
-              Register
-            </p>
-            <div
-              onClick={() => {
-                router("/auth");
-              }}
-              role="button"
-            >
-              <p>Login</p>
-            </div>
-          </div>
-        </nav>
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>LinkUp</h2>
+                </div>
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
 
-        <div className="landingMainContainer">
-          <div>
-            <h1>
-              <span style={{ color: "#FFA500" }}>Connect</span> with your loved
-              Ones
-            </h1>
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
 
-            <p>Cover a distance by LinkUp</p>
-            <div role="button">
-              <Link to={"/auth"}> <span>Get Started</span></Link>
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by LinkUp</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+
+                    <img src="/mobile.png" alt="" />
+
+                </div>
             </div>
-          </div>
-          <div>
-            <img src="/mobile.png" alt="" />
-          </div>
+
+
+
         </div>
-      </div>
-    );
+    )
 }
